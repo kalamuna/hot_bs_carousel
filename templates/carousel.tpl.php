a177bf1938b5415c9c89feeca04bf90e92d93eaf
Steven Bassett <steven.j.bassett@gmail.com>
2014-07-17 19:22:19 -0700

    Adding the last commit to the themed version of this

--- templates/carousel.tpl.php
+++ templates/carousel.tpl.php
@@ -1,33 +1,16 @@
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
+<div class="<?php print $classes ?> slide" id="<?php print $variables['id_string']; ?>" data-ride="carousel">
+  <?php print theme('carousel_indicators', array(
+    'items' => count($slides),
+    'target' => $variables['id_string'],
+  )); ?>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner">
-    <div class="item active">
-      <img src="..." alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    <div class="item">
-      <img src="..." alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    ...
+    <?php
+      print render($slides);
+    ?>
   </div>
 
   <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-  </a>
+  <?php print theme('carousel_controls',array('target'=> $variables['id_string'])); ?>
 </div>
