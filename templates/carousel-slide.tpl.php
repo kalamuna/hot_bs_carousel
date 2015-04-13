95db78d234301f846f65bd5abfc61fe32660b8d9
Josh Walker <josh@kalamuna.com>
2015-04-13 16:52:12 -0700

    Hold off rendering as late as possible to ensure maximum alterability.

--- templates/carousel-slide.tpl.php
+++ templates/carousel-slide.tpl.php
@@ -1,7 +1,7 @@
 <div class="item <?php print $active ? 'active' : '' ; ?>">
-  <?php print $image; ?>
+  <?php print render($image); ?>
   <div class="carousel-caption">
-    <h3><?php print $title; ?></h3>
-    <?php print $caption; ?>
+    <h3><?php print render($title); ?></h3>
+    <?php print render($caption); ?>
   </div>
 </div>
